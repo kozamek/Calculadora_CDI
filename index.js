66621d129d99b30774e052a2c46bee0f5fcbf0c8
Jonas Fabricio Kozamekinas <kozamekinas@gmail.com>
2023-04-30 12:54:00 -0300

    Ajuste de lista

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 var moment = require('moment');//npm install moment
 var a = moment('2023-04-10');
-var b = moment('2025-12-30');
+var b = moment('2027-12-30');
 var arr = [1000,10000,100000,1000000];// lista de valores de valores a serem simulados, vc pode colocar apenas um valor unico entre chaves.
 var selic_diaria = 0.0003739726; //Selic = 13.75%, que para CDI 13,65/100= 0,1365, e 0,1365/365 = 0,0003739726
 var count = 0;
