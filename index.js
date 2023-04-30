4577f7accd0e8ddedf3ad45ea7f87ae4b2c7bd71
Jonas Fabricio Kozamekinas <kozamekinas@gmail.com>
2023-04-30 12:57:38 -0300

    Removendo variavel valor.

--- index.js
+++ index.js
@@ -7,11 +7,10 @@ var count = 0;
 arr.forEach(funds =>{ console.log('Valor do redimento inicial com R$'+ funds);
 for (var m = moment(a); m.isBefore(b); m.add(1, 'days')) {
     funds = funds + (funds*selic_diaria);
-    var valor = funds*selic_diaria;
     count = count + 1;
     if((count%30==0)||(count==1))//if para imprimir em 30 dias e primeiro dia
     {
-      console.log('Redimento no dia '+m.format('YYYY-MM-DD') +' no valor R$'+ valor);
+      console.log('Redimento no dia '+m.format('YYYY-MM-DD') +' no valor R$'+ (funds*selic_diaria));
     }
 }
 console.log("Data final "+m.format('YYYY-MM-DD')); console.log("Redimento final total R$"+funds);
